76c0a1a6febb5d9a7ab67edf5e137f8c697f377e
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-18 19:22:11 +0800

    update slides

---
+++ src/about/experience/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="experience">
+        <h1>Experience</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
