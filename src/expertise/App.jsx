0bef8530fe42a4fc239fd0f303b742a0b9fd6361
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-19 15:23:14 +0800

    adding expertise

--- src/expertise/App.jsx
+++ src/expertise/App.jsx
@@ -5,37 +5,62 @@ function App() {
     <>
       <section className="expertise" id="expertise">
         <h1 className="">What I offer.</h1>
+        {/**
+         *
+         *
+         *
+         *
+         * */}
         <div className="container">
           <div className="offer">
             <div>
               <h5>
-                <b>01</b> Core Website Development
+                <b>01</b> Responsive Website Development
               </h5>
               <p className="description">
-                Build modern, mobile-friendly, and cross-browser compatible
-                websites.
+                Mobile-first, cross-browser compatible websites.
               </p>
             </div>
           </div>
           <div className="offer">
             <div>
               <h5>
-                <b>02</b> Optimization & Maintenance
+                <b>02</b> Custom UI/UX Implementation
               </h5>
               <p className="description">
-                Improve speed, SEO, accessibility, and overall performance for
-                better user experience.
+                Translating design mockups (Figma, Adobe XD, Sketch) into
+                functional interfaces.
               </p>
             </div>
           </div>
           <div className="offer">
             <div>
               <h5>
-                <b>03</b> UI/UX Implementation: Manual Coding
+                <b>03</b> Interactive Web Applications
               </h5>
               <p className="description">
-                Convert Figma, XD, or Sketch designs into pixel-perfect,
-                interactive interfaces.
+                Using React, Vue, Angular, or vanilla JS for dynamic
+                experiences.
+              </p>
+            </div>
+          </div>
+          <div className="offer">
+            <div>
+              <h5>
+                <b>04</b> Single Page Applications (SPA)
+              </h5>
+              <p className="description">
+                Seamless navigation with modern frameworks.
+              </p>
+            </div>
+          </div>
+          <div className="offer">
+            <div>
+              <h5>
+                <b>05</b> Landing Pages & Portfolios
+              </h5>
+              <p className="description">
+                High-converting, fast-loading, visually appealing websites.
               </p>
             </div>
           </div>
