ec0871bbfc67134ce39f4718a7ff16dd12baa094
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-19 13:06:23 +0800

    remove the links, adjusted the font size of headings, putting numbers.

--- src/expertise/App.jsx
+++ src/expertise/App.jsx
@@ -8,42 +8,36 @@ function App() {
         <div className="container">
           <div className="offer">
             <div>
-              {" "}
-              <h4>Core Website Development</h4>
+              <h5>
+                <b>01</b> Core Website Development
+              </h5>
               <p className="description">
                 Build modern, mobile-friendly, and cross-browser compatible
                 websites.
               </p>
             </div>
-            <a className="learn-more-link" href="#expertise">
-              Learn more
-            </a>
           </div>
           <div className="offer">
             <div>
-              {" "}
-              <h4>Optimization & Maintenance</h4>
+              <h5>
+                <b>02</b> Optimization & Maintenance
+              </h5>
               <p className="description">
                 Improve speed, SEO, accessibility, and overall performance for
                 better user experience.
               </p>
             </div>
-            <a className="learn-more-link" href="#expertise">
-              Learn more
-            </a>
           </div>
           <div className="offer">
             <div>
-              {" "}
-              <h4>UI/UX Implementation: Manual Coding</h4>
+              <h5>
+                <b>03</b> UI/UX Implementation: Manual Coding
+              </h5>
               <p className="description">
                 Convert Figma, XD, or Sketch designs into pixel-perfect,
                 interactive interfaces.
               </p>
             </div>
-            <a className="learn-more-link" href="#expertise">
-              Learn more
-            </a>
           </div>
         </div>
       </section>
