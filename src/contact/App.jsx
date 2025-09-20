3cf46bb9cd94b2c791b7f5e204531df07b6a6935
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-20 21:19:47 +0800

    organize code

--- src/contact/App.jsx
+++ src/contact/App.jsx
@@ -3,6 +3,21 @@ import facebook from "./assets/facebook.svg";
 import github from "./assets/github.svg";
 import instagram from "./assets/instagram.svg";
 
+const cln = {
+  span_icon: "material-symbols-rounded",
+};
+const link = {
+  location:
+    "https://www.google.com/maps?ll=7.073599,125.611055&z=14&t=m&hl=en&gl=PH&mapclient=embed&q=8000+Davao+del+Sur+Poblacion+District,+Davao+City",
+  mail: "mailto:awayanrichardjr@gmail.com?cc=richarddumaelawayanjr@gmail.com",
+  tel: "tel:+639065876602",
+  sms: "sms:+639065876602",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15837.803835093697!2d125.60075474478859!3d7.073599136864985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32f96d9e40da7c65%3A0xe94c55fbd7b32544!2sPoblacion%20District%2C%20Davao%20City%2C%208000%20Davao%20del%20Sur!5e0!3m2!1sen!2sph!4v1757508749963!5m2!1sen!2sph",
+  github: "https://github.com/awayanrichardjr",
+  fb: "https://www.facebook.com/awayanrichardjr",
+  ig: "https://www.instagram.com/awayanrichardjr/",
+};
+
 function App() {
   return (
     <>
@@ -12,29 +27,26 @@ function App() {
           <div className="container">
             <ul className="contact-details">
               <li>
-                <a
-                  href="https://www.google.com/maps?ll=7.073599,125.611055&z=14&t=m&hl=en&gl=PH&mapclient=embed&q=8000+Davao+del+Sur+Poblacion+District,+Davao+City"
-                  target="_blank"
-                >
-                  <span className="material-symbols-rounded">location_on</span>
+                <a href={link.location} target="_blank">
+                  <span className={cln.span_icon}>location_on</span>
                   <address>Davao City, 8000, Philippines</address>
                 </a>
               </li>
               <li>
-                <a href="mailto:awayanrichardjr@gmail.com?cc=richarddumaelawayanjr@gmail.com">
-                  <span className="material-symbols-rounded">mail</span>
+                <a href={link.mail}>
+                  <span className={cln.span_icon}>mail</span>
                   <address>awayanrichardjr@gmail.com</address>
                 </a>
               </li>
               <li>
-                <a href="tel:+639065876602">
-                  <span className="material-symbols-rounded">call</span>
+                <a href={link.tel}>
+                  <span className={cln.span_icon}>call</span>
                   <address>Call: +63906-587-6602</address>
                 </a>
               </li>
               <li>
-                <a href="sms:+639065876602">
-                  <span className="material-symbols-rounded">sms</span>
+                <a href={link.sms}>
+                  <span className={cln.span_icon}>sms</span>
                   <address>Text: +63906-587-6602</address>
                 </a>
               </li>
@@ -48,23 +60,17 @@ function App() {
              * */}
             <ul className="social-links">
               <li>
-                <a href="https://github.com/awayanrichardjr" target="_blank">
+                <a href={link.github} target="_blank">
                   <img src={github} alt="Github" />
                 </a>
               </li>
               <li>
-                <a
-                  href="https://www.facebook.com/awayanrichardjr"
-                  target="_blank"
-                >
+                <a href={link.fb} target="_blank">
                   <img src={facebook} alt="Facebook" />
                 </a>
               </li>
               <li>
-                <a
-                  href="https://www.instagram.com/awayanrichardjr/"
-                  target="_blank"
-                >
+                <a href={link.ig} target="_blank">
                   <img src={instagram} alt="Instagram" />
                 </a>
               </li>
@@ -72,7 +78,7 @@ function App() {
           </div>
           <div className="container">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15837.803835093697!2d125.60075474478859!3d7.073599136864985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32f96d9e40da7c65%3A0xe94c55fbd7b32544!2sPoblacion%20District%2C%20Davao%20City%2C%208000%20Davao%20del%20Sur!5e0!3m2!1sen!2sph!4v1757508749963!5m2!1sen!2sph"
+              src={link.map}
               className="map"
               // allowfullscreen=""
               loading="lazy"
--- src/header/App.jsx
+++ src/header/App.jsx
@@ -2,13 +2,25 @@ import "./App.css";
 import "./root.css";
 import profilePic from "../assets/img/me.jpg";
 
+const details = {
+  name: "Richard",
+  profession: "Frontend Developer",
+  file_link:
+    "https://drive.google.com/uc?export=download&id=1Eq5gi_scvnn4po6PWAlnaXPLu-1mq17Q",
+};
+
+const cln = {
+  croissant: "croissant-one-regular",
+  span_icon: "material-symbols-rounded",
+};
+
 function App() {
   return (
     <>
       <header className="header" id="home">
         <div className="container">
-          <h1 className="croissant-one-regular">Hi, I am Richard.</h1>
-          <h1 className="croissant-one-regular">Frontend Developer</h1>
+          <h1 className={cln.croissant}>Hi, I am {details.name}.</h1>
+          <h1 className={cln.croissant}>{details.profession}</h1>
           <p className="description">
             I build responsive and user-friendly websites.
           </p>
@@ -26,11 +38,8 @@ function App() {
               </a>
             </button>
             <button className="download-btn">
-              <a
-                href="https://drive.google.com/uc?export=download&id=1Eq5gi_scvnn4po6PWAlnaXPLu-1mq17Q"
-                download
-              >
-                <span className="material-symbols-rounded">download</span>
+              <a href={details.file_link} download>
+                <span className={cln.span_icon}>download</span>
                 <p>Download CV</p>
               </a>
             </button>
